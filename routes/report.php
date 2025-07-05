a8d02c614f538b8bb336c2582b7a6c8f8cb7cf1d
Yogesh <yogesh.xcode@gmail.com>
2025-07-05 13:33:34 +0530

    Define report route and register it via AppServiceProvider

--- routes/report.php
+++ routes/report.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\ReportController;
 
 
 Route::post('/upload/{patient_id}', [ReportController::class, 'uploadReport']);
+Route::get('/retrive/{patient_id}/{report_type?}', [ReportController::class, 'retriveReport']);
